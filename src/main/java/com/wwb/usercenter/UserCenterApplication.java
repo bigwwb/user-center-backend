e8f8c62dcbe5d0fd21f28272563f6b4502085262
吴文标 <2137585661@qq.com>
2023-04-29 18:54:45 +0800

    数据库映射

---
+++ src/main/java/com/wwb/usercenter/UserCenterApplication.java
@@ -0,0 +1,13 @@
+package com.wwb.usercenter;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.wwb.usercenter.mapper")
+public class UserCenterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserCenterApplication.class, args);
+    }
+}
